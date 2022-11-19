33146ad242a7c4adc562f9756bdc6bb0fe41d4f7
Nazmul Hasan Sani <sanikhan2016@gmail.com>
2022-11-19 12:25:30 +0600

    * serializer proto_class value updated.

--- gRPC-djnago/project/serializers/book_serializer.py
+++ gRPC-djnago/project/serializers/book_serializer.py
@@ -5,11 +5,12 @@ __author__ = "Nazmul Hasan Sani"
 
 from django_grpc_framework import proto_serializers
 
+import project_pb2
 from project.models.book import BookModel
 
 
 class BookSerializer(proto_serializers.ModelProtoSerializer):
     class Meta:
         model = BookModel
-        proto_class = 'project_pb2.BookResponse'
-        fields = '__all__'
+        proto_class = project_pb2.BookResponse
+        fields = ['id', 'name', 'author', 'type']
