f9b5ed7df351258ece84d77d80ab4f8d9e647a09
Nazmul Hasan Sani <sanikhan2016@gmail.com>
2022-11-19 12:23:47 +0600

    * service class updated.

--- gRPC-djnago/project/grpc_service/author_service.py
+++ gRPC-djnago/project/grpc_service/author_service.py
@@ -12,3 +12,27 @@ from project.serializers.author_serializer import AuthorSerializer
 class AuthorService(generics.ModelService):
     queryset = AuthorModel.objects.all()
     serializer_class = AuthorSerializer
+
+    # def List(self, request, context):
+    # """
+    # method already implemented in base class (ModelServices).If you want any custom logic you can write it by
+    #  uncommenting this method and write your custom logic.
+    # """
+
+    # def Create(self, request, context):
+    #     """
+    #     method already implemented in base class (ModelServices).If you want any custom logic you can write it by
+    #      uncommenting this method and write your custom logic.
+    #     """
+
+    # def Update(self, request, context):
+    #     """
+    #     method already implemented in base class (ModelServices).If you want any custom logic you can write it by
+    #      uncommenting this method and write your custom logic.
+    #     """
+
+    # def Destroy(self, request, context):
+    #     """
+    #     method already implemented in base class (ModelServices).If you want any custom logic you can write it by
+    #      uncommenting this method and write your custom logic.
+    #     """
--- gRPC-djnago/project/grpc_service/book_service.py
+++ gRPC-djnago/project/grpc_service/book_service.py
@@ -10,5 +10,30 @@ from project.serializers.book_serializer import BookSerializer
 
 
 class BookService(generics.ModelService):
+
     queryset = BookModel.objects.all()
     serializer_class = BookSerializer
+
+    # def List(self, request, context):
+    # """
+    # method already implemented in base class (ModelServices).If you want any custom logic you can write it by
+    #  uncommenting this method and write your custom logic.
+    # """
+
+    # def Create(self, request, context):
+    #     """
+    #     method already implemented in base class (ModelServices).If you want any custom logic you can write it by
+    #      uncommenting this method and write your custom logic.
+    #     """
+
+    # def Update(self, request, context):
+    #     """
+    #     method already implemented in base class (ModelServices).If you want any custom logic you can write it by
+    #      uncommenting this method and write your custom logic.
+    #     """
+
+    # def Destroy(self, request, context):
+    #     """
+    #     method already implemented in base class (ModelServices).If you want any custom logic you can write it by
+    #      uncommenting this method and write your custom logic.
+    #     """
